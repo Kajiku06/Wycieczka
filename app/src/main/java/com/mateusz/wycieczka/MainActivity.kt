3902f45073df9894eca9a58050ecb602c9f2dd47
jp2137 <sbaiyassine2004@gmail.com>
2022-12-06 09:09:40 +0100

    Initial commit

---
+++ app/src/main/java/com/mateusz/wycieczka/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mateusz.wycieczka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Calendar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
